78f47b087641721b60ae30afbdb808fd201c5994
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-10-27 21:58:59 +0000

    Removed comment

--- run_test_and_coverage.sh
+++ run_test_and_coverage.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-#pytest && coverage run --branch --source game.py game_runner.py game_test.py && coverage report -m && coverage html
 pytest && coverage run --branch game.py game_runner.py game_test.py && coverage report -m && coverage html
