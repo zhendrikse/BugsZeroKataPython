5ce6a348cf6b1a8d5d79b4dfb5ad7248c9856564
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-11-18 19:16:42 +0000

    Improvements

--- game_test.py
+++ game_test.py
@@ -5,7 +5,7 @@ from subprocess import run
 class GameTest(unittest.TestCase):
     def test_trivia_game(self):
         output = run(["python", "game_runner.py"], capture_output=True).stdout
-        verify(output)
+        verify(output.decode())
 
 if __name__ == "__main__":
     unittest.main()
